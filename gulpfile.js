f414a951891598b5e26ec341a64d6e5fbe81e37a
Dmitry <dmitrixkovalenko@gmail.com>
2019-11-19 15:20:20 +0200

    The first version of the project

---
+++ gulpfile.js
@@ -0,0 +1,119 @@
+var gulp = require('gulp'),
+    cleanCSS = require('gulp-clean-css'),
+    autoprefixer = require('gulp-autoprefixer'),
+    imagemin = require('gulp-imagemin'),
+    JSminify = require('gulp-minify'),
+    concat = require('gulp-concat'),
+    useref = require('gulp-useref'),
+    browserSync = require('browser-sync').create(),
+    cache = require('gulp-cache'),
+    del = require('del'),
+    sass = require('gulp-sass'),
+    iconfont    =   require( 'gulp-iconfont' ),
+    iconfontCss =   require( 'gulp-iconfont-css' );
+
+sass.compiler = require('node-sass');
+
+gulp.task('sass', function () {
+    return gulp.src('app/sass/**/*.sass')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('app/css'));
+});
+
+
+gulp.task('clean', function (callback) {
+    del('dist');
+    return cache.clearAll(callback);
+});
+
+gulp.task('not_cached_image_min', function () {
+    return gulp.src('app/assets/img/**/*.+(png|jpg|jpeg|gif|svg)')
+    // кэширование изображений, прошедших через imagemin
+        .pipe(cache(imagemin()))
+        .pipe(gulp.dest('dist/assets/img'))
+});
+
+gulp.task('export', function () {
+    return gulp.src('app/assets/fonts/**/*')
+        .pipe(gulp.dest('dist/assets/fonts'))
+});
+
+// Static Server + watching css/js/html files
+gulp.task('server', function () {
+
+    browserSync.init({
+        server: "app/"
+    });
+
+    gulp.watch('app/sass/**/*.sass',  gulp.series('sass'));
+    gulp.watch('app/sass/**/*.scss',  gulp.series('sass'));
+
+    gulp.watch("app/css/*.css", function () {
+        return gulp.src("app/css/*.css").pipe(browserSync.stream());
+    });
+    gulp.watch("app/js/*.js", function () {
+        return gulp.src("app/js/*.js").pipe(browserSync.reload());
+    });
+    gulp.watch("app/*.html").on('change', browserSync.reload);
+});
+
+gulp.task('compress_js', function () {
+    return gulp.src(['app/js/*.js'])
+        .pipe(JSminify({
+            ext: {
+                min: '.js'
+            }, noSource: true
+        }))
+        .pipe(concat('main.min.js'))
+        .pipe(gulp.dest('dist/js'))
+});
+
+gulp.task('html_rebuild', function () {
+    return gulp.src('app/*.html')
+        .pipe(useref())
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('image_min', function () {
+    return gulp.src('app/assets/img/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/assets/img'))
+});
+
+gulp.task('minCSS', function () {
+    return gulp.src('app/css/**/*.css')
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(autoprefixer({
+            cascade: false
+        }))
+        .pipe(gulp.dest('dist/css'));
+});
+
+// icon fonts
+var fontName = 'icons';
+
+// add svg icons to the folder "icons" and use 'iconfont' task for generating icon font
+gulp.task( 'iconfont', async () => {
+    gulp.src( 'app/assets/icons/*.svg' )
+    .pipe( iconfontCss( {
+        // где будет наш scss файл
+        targetPath   : '../../sass/_icons.scss',
+        // пути подлючения шрифтов в _icons.scss
+        fontPath     : '../assets/fonts/',
+        fontName    : fontName
+
+    } ) )
+    .pipe( iconfont( {
+        fontName    : fontName,
+        formats     : [ 'svg', 'ttf', 'eot', 'woff', 'woff2' ],
+        normalize   : true,
+        fontHeight  : 1001
+    } ) )
+    .pipe( gulp.dest( 'app/assets/fonts' ) )
+});
+
+
+
+
+gulp.task('build', gulp.series('minCSS', 'compress_js', 'not_cached_image_min', 'html_rebuild', 'export'));
+gulp.task('default', gulp.series('sass', 'server'));
